868099670ad2c9a34cfbdbf39121273a030ebd61
bankonly <for21bn01@gmail.com>
2022-08-28 23:02:25 +0700

    s

--- users.go
+++ users.go
@@ -7,25 +7,25 @@ var (
 )
 
 type UserModel struct {
-	ID             primitive.ObjectID `json:"_id" bson:"_id"`
-	Firstname      string             `json:"first_name" bson:"first_name"`
-	Lastname       string             `json:"last_name" bson:"last_name"`
-	PhoneNumber    string             `json:"phone_number" bson:"phone_number"`
+	ID             primitive.ObjectID `json:"_id" bson:"_id" validate:"required"`
+	Firstname      string             `json:"first_name" bson:"first_name" validate:"required"`
+	Lastname       string             `json:"last_name" bson:"last_name" validate:"required"`
+	PhoneNumber    string             `json:"phone_number" bson:"phone_number" validate:"required"`
 	Avatar         string             `json:"avartar" bson:"avartar"`
 	Cover          string             `json:"cover" bson:"cover"`
-	Email          string             `json:"email" bson:"email"`
+	Email          string             `json:"email" bson:"email" validate:"required"`
 	IsActive       bool               `json:"is_active" bson:"is_active"`
-	BirthDate      primitive.DateTime `json:"birth_date" bson:"birth_date"`
-	Role           string             `json:"role" bson:"role"`
-	Position       string             `json:"position" bson:"position"`
-	VillageId      primitive.ObjectID `json:"village_id" bson:"village_id"`
-	IdNo           string             `json:"id_no" bson:"id_no"`
+	BirthDate      primitive.DateTime `json:"birth_date" bson:"birth_date" validate:"required"`
+	Role           string             `json:"role" bson:"role" validate:"required"`
+	Position       string             `json:"position" bson:"position" validate:"required"`
+	VillageId      primitive.ObjectID `json:"village_id" bson:"village_id" validate:"required"`
+	IdNo           string             `json:"id_no" bson:"id_no" validate:"required"`
 	DeviceToken    string             `json:"device_token" bson:"device_token"`
-	VillageBirthId primitive.ObjectID `json:"village_birth_id" bson:"village_birth_id"`
-	Gender         string             `json:"gender" bson:"gender"`
+	VillageBirthId primitive.ObjectID `json:"village_birth_id" bson:"village_birth_id" validate:"required"`
+	Gender         string             `json:"gender" bson:"gender" validate:"required"`
 	Height         float32            `json:"height" bson:"height"`
 	Weight         float32            `json:"weight" bson:"weight"`
 	National       string             `json:"national" bson:"national"`
 	Region         string             `json:"region" bson:"region"`
-	Password       string             `json:"password" bson:"password"`
+	Password       string             `json:"password" bson:"password" validate:"required"`
 }
