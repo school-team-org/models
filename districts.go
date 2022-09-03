d19c1f0e16b523fb4ac6166d2585c9f821b5fbed
bankonly <bank@techcsl.com>
2022-09-03 12:05:41 +0700

    1.1.0

--- districts.go
+++ districts.go
@@ -1,11 +1,14 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 var (
 	DISTRICT_MODEL_NAME = "districts"
 )
 
 type DistrictModel struct {
 	DefaultField `bson:",inline"`
-	NameLA       string `json:"name_la" bson:"name_la" validate:"required"`
-	NameEn       string `json:"name_en" bson:"name_en" validate:"required"`
+	NameLA       string             `json:"name_la" bson:"name_la" validate:"required"`
+	NameEn       string             `json:"name_en" bson:"name_en" validate:"required"`
+	ProvinceID   primitive.ObjectID `json:"province_id" bson:"province_id" validate:"required"`
 }
--- villages.go
+++ villages.go
@@ -1,11 +1,14 @@
 package models
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 var (
 	VILLAGE_MODEL_NAME = "districts"
 )
 
 type VillageModel struct {
 	DefaultField `bson:",inline"`
-	NameLA       string `json:"name_la" bson:"name_la" validate:"required"`
-	NameEn       string `json:"name_en" bson:"name_en" validate:"required"`
+	NameLA       string             `json:"name_la" bson:"name_la" validate:"required"`
+	NameEn       string             `json:"name_en" bson:"name_en" validate:"required"`
+	DistrictID   primitive.ObjectID `json:"district_id" bson:"district_id" validate:"required"`
 }
