b1e9c7abddf15dbbccc8e87d18f4ab8047aa9860
bankonly <for21bn01@gmail.com>
2022-09-04 21:45:05 +0700

    1.2.6

--- villages.go
+++ villages.go
@@ -7,7 +7,7 @@ var (
 )
 
 type VillageModel struct {
-	DefaultField `bson:",inline" json:"default_field"`
+	DefaultField `bson:",inline"`
 	NameLA       string             `json:"name_la" bson:"name_la" validate:"required"`
 	NameEn       string             `json:"name_en" bson:"name_en" validate:"required"`
 	DistrictID   primitive.ObjectID `json:"district_id" bson:"district_id" validate:"required"`
