2ca648fb17b56b14398afe514a087f58a2e7e1b7
bankonly <for21bn01@gmail.com>
2022-09-17 22:38:49 +0700

    1.2.14

--- users.go
+++ users.go
@@ -2,8 +2,6 @@ package models
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var (
@@ -14,24 +12,24 @@ var (
 
 type UserModel struct {
 	DefaultField   `bson:",inline"`
-	Firstname      string             `json:"first_name" bson:"first_name" form:"first_name" validate:"required"`
-	Lastname       string             `json:"last_name" bson:"last_name" form:"last_name" validate:"required"`
-	PhoneNumber    string             `json:"phone_number" bson:"phone_number" form:"phone_number" validate:"required"`
-	Avatar         string             `json:"avartar" bson:"avartar" form:"avartar"`
-	Cover          string             `json:"cover" bson:"cover" form:"cover"`
-	Email          string             `json:"email" bson:"email" form:"email" validate:"required"`
-	IsActive       bool               `json:"is_active" bson:"is_active" form:"is_active"`
-	BirthDate      time.Time          `json:"birth_date" bson:"birth_date" form:"birth_date" validate:"required"`
-	Role           string             `json:"role" bson:"role" form:"role" validate:"required"`
-	Position       string             `json:"position" bson:"position" form:"position" validate:"required"`
-	VillageId      primitive.ObjectID `json:"village_id" bson:"village_id" form:"village_id" validate:"required"`
-	IdNo           string             `json:"id_no" bson:"id_no" form:"id_no" validate:"required"`
-	DeviceToken    string             `json:"device_token" bson:"device_token" form:"device_token"`
-	VillageBirthId primitive.ObjectID `json:"village_birth_id" bson:"village_birth_id" form:"village_birth_id" validate:"required"`
-	Gender         string             `json:"gender" bson:"gender" form:"gender" validate:"required"`
-	Height         float32            `json:"height" bson:"height" form:"height"`
-	Weight         float32            `json:"weight" bson:"weight" form:"weight"`
-	National       string             `json:"national" bson:"national" form:"national"`
-	Region         string             `json:"region" bson:"region" form:"region"`
-	Password       string             `json:"password" bson:"password" form:"password" validate:"required"`
+	Firstname      string    `json:"first_name" bson:"first_name" form:"first_name" validate:"required"`
+	Lastname       string    `json:"last_name" bson:"last_name" form:"last_name" validate:"required"`
+	PhoneNumber    string    `json:"phone_number" bson:"phone_number" form:"phone_number" validate:"required"`
+	Avatar         string    `json:"avartar" bson:"avartar" form:"avartar"`
+	Cover          string    `json:"cover" bson:"cover" form:"cover"`
+	Email          string    `json:"email" bson:"email" form:"email" validate:"required"`
+	IsActive       bool      `json:"is_active" bson:"is_active" form:"is_active"`
+	BirthDate      time.Time `json:"birth_date" bson:"birth_date" form:"birth_date" validate:"required"`
+	Role           string    `json:"role" bson:"role" form:"role" validate:"required"`
+	Position       string    `json:"position" bson:"position" form:"position" validate:"required"`
+	VillageId      string    `json:"village_id" bson:"village_id" form:"village_id" validate:"required,uuidv4"`
+	IdNo           string    `json:"id_no" bson:"id_no" form:"id_no" validate:"required"`
+	DeviceToken    string    `json:"device_token" bson:"device_token" form:"device_token"`
+	VillageBirthId string    `json:"village_birth_id" bson:"village_birth_id" form:"village_birth_id" validate:"required,uuidv4"`
+	Gender         string    `json:"gender" bson:"gender" form:"gender" validate:"required"`
+	Height         float32   `json:"height" bson:"height" form:"height"`
+	Weight         float32   `json:"weight" bson:"weight" form:"weight"`
+	National       string    `json:"national" bson:"national" form:"national"`
+	Region         string    `json:"region" bson:"region" form:"region"`
+	Password       string    `json:"password" bson:"password" form:"password" validate:"required"`
 }
