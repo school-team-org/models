89e749e970a61d9bc47fa45966075056a4396d26
bankonly <for21bn01@gmail.com>
2022-09-17 23:03:56 +0700

    1.2.16

--- users.go
+++ users.go
@@ -12,24 +12,24 @@ var (
 
 type UserModel struct {
 	DefaultField   `bson:",inline"`
-	Firstname      string    `json:"first_name" bson:"first_name" form:"first_name" validate:"required"`
-	Lastname       string    `json:"last_name" bson:"last_name" form:"last_name" validate:"required"`
-	PhoneNumber    string    `json:"phone_number" bson:"phone_number" form:"phone_number" validate:"required"`
+	Firstname      string    `json:"first_name" bson:"first_name" form:"first_name" valid:"required"`
+	Lastname       string    `json:"last_name" bson:"last_name" form:"last_name" valid:"required"`
+	PhoneNumber    string    `json:"phone_number" bson:"phone_number" form:"phone_number" valid:"required"`
 	Avatar         string    `json:"avartar" bson:"avartar" form:"avartar"`
 	Cover          string    `json:"cover" bson:"cover" form:"cover"`
-	Email          string    `json:"email" bson:"email" form:"email" validate:"required"`
+	Email          string    `json:"email" bson:"email" form:"email" valid:"required"`
 	IsActive       bool      `json:"is_active" bson:"is_active" form:"is_active"`
-	BirthDate      time.Time `json:"birth_date" bson:"birth_date" form:"birth_date" validate:"required"`
-	Role           string    `json:"role" bson:"role" form:"role" validate:"required"`
-	Position       string    `json:"position" bson:"position" form:"position" validate:"required"`
-	VillageId      string    `json:"village_id" bson:"village_id" form:"village_id" validate:"required,uuid4"`
-	IdNo           string    `json:"id_no" bson:"id_no" form:"id_no" validate:"required"`
+	BirthDate      time.Time `json:"birth_date" bson:"birth_date" form:"birth_date" valid:"required"`
+	Role           string    `json:"role" bson:"role" form:"role" valid:"required"`
+	Position       string    `json:"position" bson:"position" form:"position" valid:"required"`
+	VillageId      string    `json:"village_id" bson:"village_id" form:"village_id" valid:"required,uuid4"`
+	IdNo           string    `json:"id_no" bson:"id_no" form:"id_no" valid:"required"`
 	DeviceToken    string    `json:"device_token" bson:"device_token" form:"device_token"`
-	VillageBirthId string    `json:"village_birth_id" bson:"village_birth_id" form:"village_birth_id" validate:"required,uuid4"`
-	Gender         string    `json:"gender" bson:"gender" form:"gender" validate:"required"`
+	VillageBirthId string    `json:"village_birth_id" bson:"village_birth_id" form:"village_birth_id" valid:"required,uuid4"`
+	Gender         string    `json:"gender" bson:"gender" form:"gender" valid:"required"`
 	Height         float32   `json:"height" bson:"height" form:"height"`
 	Weight         float32   `json:"weight" bson:"weight" form:"weight"`
 	National       string    `json:"national" bson:"national" form:"national"`
 	Region         string    `json:"region" bson:"region" form:"region"`
-	Password       string    `json:"password" bson:"password" form:"password" validate:"required"`
+	Password       string    `json:"password" bson:"password" form:"password" valid:"required"`
 }
